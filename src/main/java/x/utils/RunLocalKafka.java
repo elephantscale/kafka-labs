38c2e9b5ab2f6f9d2a76c240e6ef7d642d281f8c
Sujee Maniyam (sujee-mac2) <sujee@sujee.net>
2016-07-25 00:32:35 -0700

    added lab 3

--- src/main/java/x/utils/RunLocalKafka.java
+++ src/main/java/x/utils/RunLocalKafka.java
@@ -3,17 +3,6 @@ package x.utils;
 import java.nio.file.Files;
 import java.util.Properties;
 
-import org.I0Itec.zkclient.ZkClient;
-
-import kafka.server.KafkaConfig;
-import kafka.server.KafkaServer;
-import kafka.utils.MockTime;
-import kafka.utils.TestUtils;
-import kafka.utils.Time;
-import kafka.utils.ZKStringSerializer$;
-import kafka.utils.ZkUtils;
-import kafka.zk.EmbeddedZookeeper;
-
 public class RunLocalKafka {
   private static final String ZKHOST = "127.0.0.1";
   private static final int ZKPORT = 2181;
@@ -22,35 +11,30 @@ public class RunLocalKafka {
   private static final String TOPIC = "test";
 
   public static void main(String[] args) throws Exception {
-    //test2();
-    test1();
-    
-  }
-  public static void test1() throws Exception {
+
     try {
       Properties zkProperties = new Properties();
-      //zkProperties.setProperty("dataDir", "/tmp/zookeeper");
-      zkProperties.setProperty("dataDir", Files.createTempDirectory("kafka-").toAbsolutePath().toString());
+      // zkProperties.setProperty("dataDir", "/tmp/zookeeper");
+      zkProperties.setProperty("dataDir",
+          Files.createTempDirectory("kafka-").toAbsolutePath().toString());
       zkProperties.setProperty("clientPort", "" + ZKPORT);
 
       Properties kafkaProperties = new Properties();
       kafkaProperties.setProperty("zookeeper.connect", ZKHOST + ":" + ZKPORT);
       kafkaProperties.setProperty("broker.id", "0");
       kafkaProperties.setProperty("log.dirs", "/tmp/kafka-logs");
-      //kafkaProperties.setProperty("log.dirs", Files.createTempDirectory("kafka-").toAbsolutePath().toString());
+      // kafkaProperties.setProperty("log.dirs",
+      // Files.createTempDirectory("kafka-").toAbsolutePath().toString());
 
       kafkaProperties.setProperty("listeners",
           "PLAINTEXT://" + KAFKA_HOST + ":" + KAFKA_PORT);
       
       KafkaLocal kafka = new KafkaLocal(kafkaProperties, zkProperties);
       Thread.sleep(5000);
+      kafka.stop();
     } catch (Exception e) {
       e.printStackTrace();
     }
 
-  }
-  
-  public static void test2 () throws Exception {
-   
   }
 }
