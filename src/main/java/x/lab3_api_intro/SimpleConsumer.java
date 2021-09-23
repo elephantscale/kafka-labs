f963bfcf29fe938796b814c1f1c59be5c711ea04
Sujee Maniyam <sujee@elephantscale.com>
2021-09-22 17:48:33 -0700

    updated pom.xml and code sample

--- src/main/java/x/lab3_api_intro/SimpleConsumer.java
+++ src/main/java/x/lab3_api_intro/SimpleConsumer.java
@@ -4,21 +4,31 @@ import java.util.Arrays;
 import java.util.Properties;
 import java.time.Duration;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class SimpleConsumer {
 	private static final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
 
 	public static void main(String[] args) throws Exception {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
-		props.put("group.id", "group1");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		
+		/*
+		// Try the following to read-from-beginning
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "group2");
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,  "false");
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		*/
+		
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 		consumer.subscribe(Arrays.asList("test")); // subscribe to topics
 
--- src/main/java/x/lab3_api_intro/SimpleProducer.java
+++ src/main/java/x/lab3_api_intro/SimpleProducer.java
@@ -3,33 +3,43 @@ package x.lab3_api_intro;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import x.utils.MyConfig;
 
 public class SimpleProducer {
 	private static final Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
 
   public static void main(String[] args) throws Exception {
     Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092");
-    props.put("client.id", "SimpleProducer");
-    props.put("key.serializer",
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ProducerConfig.CLIENT_ID_CONFIG, "SimpleProducer");
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
         "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("value.serializer",
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
         "org.apache.kafka.common.serialization.StringSerializer");
 
     KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
-    String topic = MyConfig.TOPIC_TEST; // "test"
-    String key = new Integer(1).toString();
+    String topic = "test";
+    String key = "" + System.currentTimeMillis();
     String value = "Hello world";
     ProducerRecord<String, String> record =
         new ProducerRecord<>(topic, key, value);
-    logger.debug("sending : " + record);
-    producer.send(record);
+    
+    long t1 = System.nanoTime();
+    RecordMetadata meta = producer.send(record).get();
+    long t2 = System.nanoTime();
+    
+    logger.debug(String.format("Sent record (key:%s, value:%s), "
+    		+ "meta (partition=%d, offset=%d, timestamp=%d), "
+    		+ "time took = %.2f ms",
+    		key, value, meta.partition(), meta.offset(), meta.timestamp(),
+    		(t2-t1)/1e6));
+
 
     producer.close();
 
