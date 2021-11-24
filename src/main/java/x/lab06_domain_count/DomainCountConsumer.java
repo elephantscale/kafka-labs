2cfba024f91b911fe7d23dfa6be57b31805d7c7a
Sujee Maniyam <sujee@elephantscale.com>
2021-11-24 02:35:39 -0800

    lab 6 - domain count updated

--- src/main/java/x/lab06_domain_count/DomainCountConsumer.java
+++ src/main/java/x/lab06_domain_count/DomainCountConsumer.java
@@ -21,56 +21,55 @@ import x.utils.MyConfig;
 public class DomainCountConsumer {
 	private static final Logger logger = LoggerFactory.getLogger(DomainCountConsumer.class);
 
-  public static void main(String[] args) throws Exception {
-    Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092");
-    props.put("group.id", "group1");
-    props.put("key.deserializer",
-        "org.apache.kafka.common.serialization.StringDeserializer");
-    props.put("value.deserializer",
-        "org.apache.kafka.common.serialization.StringDeserializer");
-    KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-    consumer.subscribe(Arrays.asList(MyConfig.TOPIC_CLICKSTREAM)); // subscribe
-                                                                  // to topics
-    Map<String, Integer> domainCount = new HashMap<>();
-    Gson gson = new Gson();
-    boolean keepRunning = true;
+	public static void main(String[] args) throws Exception {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id", "group1");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		consumer.subscribe(Arrays.asList(MyConfig.TOPIC_CLICKSTREAM)); // subscribe
+																		// to topics
+		Map<String, Integer> domainCount = new HashMap<>();
+		Gson gson = new Gson();
+		boolean keepRunning = true;
+		logger.info("Listening on topic: " + MyConfig.TOPIC_CLICKSTREAM);
 		while (keepRunning) {
 			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(0));
-      if (records.count() == 0) continue;
-			logger.debug ("Got " + records.count() + " messages");
+			if (records.count() == 0)
+				continue;
+			logger.debug("Got " + records.count() + " messages");
 			for (ConsumerRecord<String, String> record : records) {
-				try{
+				try {
 					logger.debug("Received message : " + record);
 
-					//# TODO-1 : extract the JSON string from record
-					//# Hint : record.value()
+					// # TODO-1 : extract the JSON string from record
+					// # Hint : record.value()
 					String clickstreamJSON = "";
 
-					ClickstreamData clickstream =
-					gson.fromJson(clickstreamJSON, ClickstreamData.class);
+					ClickstreamData clickstream = gson.fromJson(clickstreamJSON, ClickstreamData.class);
 
-					//# TODO-2 : extract the domain attribute
-					//# get it from key : record.key()
-					//# or get it from clickstream  object
+					// # TODO-2 : extract the domain attribute
+					// # get it from key : record.key()
+					// # or get it from clickstream object
 					String domain = "???";
+					
+					logger.debug("Got domain: " + domain);
 
-					//# TODO-3  figure out a way to count domains
-					//# Hint : Use a HashMap
+					// # TODO-3 figure out a way to count domains
+					// # Hint : Use a HashMap
 					// int currentCount = domainCount.getOrDefault(domain, 0);
-					//# update the count
+					// # update the count
 					// domainCount.put (domain, ??? + 1);
 
-					//# pretty print hashmap
-					logger.info("Domain Count is \n"
-					+ Arrays.toString(domainCount.entrySet().toArray()));
+					// # pretty print hashmap
+					logger.info("Domain Count is \n" + Arrays.toString(domainCount.entrySet().toArray()));
 
-				}
-				catch (Exception ex) {
+				} catch (Exception ex) {
 					logger.error("", ex);
 				}
 			} // end for
 		} // end while
-    consumer.close();
-  }
+		consumer.close();
+	}
 }
